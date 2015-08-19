1305c9392e901f3bdf26dd0cbe44fe9e0247f89e
Andrew Gacek <andrew.gacek@gmail.com>
2015-08-19 15:43:08 -0500

    Clean up script

--- demo.sh
+++ demo.sh
@@ -5,11 +5,18 @@ set -x
 USB_PORT=${1-/dev/ttyUSB0}
 UDP_PORT=${2-4000}
 
+# Start in top left
+xdotool key Ctrl+Alt+Left
+xdotool key Ctrl+Alt+Up
+
+# Open GCS and CAMERA terminals
 gnome-terminal -t GCS --working-directory=smaccmpilot-build/smaccmpilot-stm32f4/src/smaccm-comm-client -x bash -c "./gcs.sh $USB_PORT 57600 --verbose"
 gnome-terminal -t CAMERA --working-directory=camera_demo -x java -jar SmaccmViewer.jar $UDP_PORT
+
+# Open Firefox for HUD
 firefox localhost:8080/hud.html &
 
-sleep 3
+sleep 5
 
 # Move camera terminal to workspace 3
 xdotool search CAMERA windowactivate key Ctrl+Alt+Shift+Down
@@ -27,3 +34,8 @@ sleep 1
 # Clear Ubuntu workspace manager
 xdotool key Ctrl+Alt+Left
 xdotool key Ctrl+Alt+Up
+
+# Refresh Firefox
+sleep 10
+xdotool search "Problem loading page" windowactivate key Ctrl+r
+xdotool search "SMACCMPilot" windowactivate key Ctrl+r
